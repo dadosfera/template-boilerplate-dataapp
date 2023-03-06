0eddbd0f504502b9ea4afcf56ab83e621bc06b9c
Victor Radael <victorradael+github@gmail.com>
2023-03-06 12:34:08 -0300

    DOC: Att

--- app/pages/2 Data Validation.py
+++ app/pages/2 Data Validation.py
@@ -1,16 +1,27 @@
 import streamlit as st
 from pydantic import BaseModel, Field, HttpUrl
 from pydantic.color import Color
-
 import streamlit_pydantic as sp
 
-
 class ExampleModel(BaseModel):
+    """
+    Example Pydantic model for a form-based data app.
+
+    Attributes:
+        url (HttpUrl): A URL field.
+        color (Color): A color field.
+        email (str): An email field with a maximum length of 100 and a regex pattern to validate the email format.
+    """
     url: HttpUrl
     color: Color
     email: str = Field(..., max_length=100, regex=r"^\S+@\S+$")
 
-
 data = sp.pydantic_form(key="my_form", model=ExampleModel)
 if data:
+    """
+    Displays the submitted data as a JSON object.
+
+    Args:
+        data (ExampleModel): A Pydantic model instance representing the submitted form data.
+    """
     st.json(data.json())
--- app/pages/3 Editable Dataframe.py
+++ app/pages/3 Editable Dataframe.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+# Create a pandas DataFrame with some example data
 df = pd.DataFrame(
     [
         {"command": "st.selectbox", "rating": 4, "is_widget": True},
@@ -9,7 +10,19 @@ df = pd.DataFrame(
     ]
 )
 
-edited_df = st.experimental_data_editor(df, num_rows= "dynamic", use_container_width=True) # 👈 An editable dataframe
+# Create an editable dataframe using the Streamlit data editor
+edited_df = st.experimental_data_editor(df, num_rows="dynamic", use_container_width=True)
+# Arguments:
+# - df (pandas.DataFrame): The dataframe to be edited.
+# - num_rows (int or str): The number of rows to display in the editor. If "dynamic", the editor will display all rows.
+# - use_container_width (bool): Whether to use the full width of the Streamlit app.
+# Returns: pandas.DataFrame. The edited dataframe.
+# Raises: ValueError if num_rows is not "dynamic" and is not a positive integer.
 
+# Find the command with the highest rating in the edited dataframe
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈") 
+# Returns: str. The command with the highest rating.
+
+# Display the favorite command as a Markdown string
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+# Arguments: favorite_command (str). The command with the highest rating.
